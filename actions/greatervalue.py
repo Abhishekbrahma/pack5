e38c6608a6c1960223481d8fea47c7e57c23459f
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-26 17:10:00 +0530

    type changes

--- actions/greatervalue.py
+++ actions/greatervalue.py
@@ -4,12 +4,12 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, a,b,c,d,e):
-        list=[]
-        list.append(a)
-        list.append(b)
-        list.append(c)
-        list.append(d)
-        list.append(e)
+        samplelist=list()        
+        samplelist.append(a)
+        samplelist.append(b)
+        samplelist.append(c)
+        samplelist.append(d)
+        samplelist.append(e)
         
         print(list)
         return(True,list)
