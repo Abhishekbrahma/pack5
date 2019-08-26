c4b914431caf7c12d989189f0fe778f68b073aa9
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-26 17:27:44 +0530

    type changes

--- actions/greatervalue.py
+++ actions/greatervalue.py
@@ -5,11 +5,11 @@ class MyAction(Action):
 
     def run(self, a):
         if a>6:
-            for num in range(a):
-            for i in range(num):
-            print (num, end="")
-            print("\n")
-            return(True,a) 
+        for num in range(a):
+        for i in range(num):
+        print (num, end="")
+        print("\n")
+        return(True,a)
         else:
         print("data will be greater than 6")
         return(True,a)       
