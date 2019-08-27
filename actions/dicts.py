bf66b4d9a72f42cdfff961ae2ad6be993e6aaf88
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-27 16:38:38 +0530

    changes done

--- actions/dicts.py
+++ actions/dicts.py
@@ -4,7 +4,7 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, a,b,c,d,e,f,g,h):
-        Dicttest = dict{}
+        Dicttest = dict()
         Dicttest.update({"test":2})
         print(Dicttest)
         return(True,Dicttest)
