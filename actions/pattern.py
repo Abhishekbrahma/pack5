61c5c9532382a3a283d0286df518f6b090dc25b8
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-27 15:31:07 +0530

    changes done

--- actions/pattern.py
+++ actions/pattern.py
@@ -4,6 +4,7 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, a):
-        for num in range(a):
-            print(num)
+        for num in range(0,a):
+            for j in range(0,i+1):
+                print("* ",end=" ")
         return(True,a)
