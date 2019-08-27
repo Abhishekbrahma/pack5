421717fd377542d9c9fd9adbc36de53ea2befcbb
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-27 16:36:54 +0530

    changes done

--- actions/dicts.py
+++ actions/dicts.py
@@ -5,6 +5,6 @@ class MyAction(Action):
 
     def run(self, a,b,c,d,e,f,g,h):
         Dicttest = dict{}
-        Dicttest.update({a:b,c:d,e:f,g:h})
+        Dicttest.update({"test":2})
         print(Dicttest)
         return(True,Dicttest)
