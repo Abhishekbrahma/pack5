42cb9cc95033d45752c608a75600bee8ed2f0887
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-27 15:54:33 +0530

    changes done

--- actions/pattern.py
+++ actions/pattern.py
@@ -6,6 +6,6 @@ class MyAction(Action):
     def run(self, a):
         for num in range(0,a):
             for j in range(0,num+1):
-                print(j)
+                print(j, end=' ')
             print("\r")
         return(True,a)
