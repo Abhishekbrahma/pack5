145b77f4186663d0e1931a440bf23ad93bb2be69
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-28 16:01:42 +0530

    dynamic

--- actions/dynamicdicts.py
+++ actions/dynamicdicts.py
@@ -5,12 +5,7 @@ class MyAction(Action):
     def run(self, a,b,c,d,e,f,g,h,i):
         Dicttest = dict()
         if b in a:
-            Dicttest.update({a:{b:c},d:e,f:g,h:i)
-        elif d in a:
-            Dicttest.update({a:{d:e},b:c,f:g,h:i)
-        elif f in a:
-             Dicttest.update({a:{f:g},b:c,d:e,h:i)
-        elif h in a:
-             Dicttest.update({a:{h:i},b:c,d:e,f:g)
+             Dicttest.update({a:{b:c},d:e,f:g,h:i)
+           
         print(Dicttest)
         return(True,Dicttest)
