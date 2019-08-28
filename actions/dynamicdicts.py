b48702e82004bdddeee251f22acf3867eaa4b924
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-28 16:23:47 +0530

    dynamic

--- actions/dynamicdicts.py
+++ actions/dynamicdicts.py
@@ -3,9 +3,14 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
     def run(self, a,b,c,d,e,f,g,h,i):
+        checkstr=a.join(b)
         Dicttest = dict()
-        if a.find(b):
-             Dicttest.update({a:{b:c},d:e,f:g,h:i)
+        if a.startswith(b):
+            Dicttest.update({a:{b:c},d:e,f:g,h:i})
+        elif c.startswith(d):
+            Dicttest.update({a:b,c:{d:e},f:g,h:i})
+        elif e.startswith(f):
+            Dicttest.update({a:b,c:d,e:{f:g},h:i})
            
         print(Dicttest)
         return(True,Dicttest)
