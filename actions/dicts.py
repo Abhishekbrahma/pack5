36d29af336d9463465e5a51f413aa756fb16017b
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-28 15:48:16 +0530

    dynamic

--- actions/dicts.py
+++ actions/dicts.py
@@ -2,9 +2,8 @@ import sys
 from st2common.runners.base_action import Action
 
 class MyAction(Action):
-
-    def run(self, a,b,c,d,e,f,g,h,i):
+    def run(self, a,b,c,d,e,f,g,h):
         Dicttest = dict()
-        Dicttest.update({a:{b:c,d:e},f:g,h:i})
+        Dicttest.update({a:b,c:d,e:f,g:h})
         print(Dicttest)
         return(True,Dicttest)
---
+++ actions/dynamicdicts.py
@@ -0,0 +1,16 @@
+import sys
+from st2common.runners.base_action import Action
+
+class MyAction(Action):
+    def run(self, a,b,c,d,e,f,g,h,i):
+        Dicttest = dict()
+        if b in a:
+            Dicttest.update({a:{b:c},d:e,f:g,h:i)
+        elif d in a:
+            Dicttest.update({a:{d:e},b:c,f:g,h:i)
+        elif f in a:
+             Dicttest.update({a:{f:g},b:c,d:e,h:i)
+        elif h in a:
+             Dicttest.update({a:{h:i},b:c,d:e,f:g,h:i)
+        print(Dicttest)
+        return(True,Dicttest)
