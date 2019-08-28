f186edd089126bbf4f099a5b98f4db4b61980edc
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-08-28 16:04:09 +0530

    dynamic

--- actions/dynamicdicts.py
+++ actions/dynamicdicts.py
@@ -4,7 +4,7 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
     def run(self, a,b,c,d,e,f,g,h,i):
         Dicttest = dict()
-        if b in a:
+        if a.find(b):
              Dicttest.update({a:{b:c},d:e,f:g,h:i)
            
         print(Dicttest)
